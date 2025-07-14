20fd7720f1b2448553f02e87e54f015febf95d41
Weverton <wevertonmartinsdigital@gmail.com>
2025-07-14 09:42:22 -0300

    Add files via upload

---
+++ sistemas-bancario/src/main/java/com/brasil/devlops/sistemas_bancario/SistemasBancarioApplication.java
@@ -0,0 +1,13 @@
+package com.brasil.devlops.sistemas_bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasBancarioApplication.class, args);
+	}
+
+}
---
+++ sistemas-bancario/src/test/java/com/brasil/devlops/sistemas_bancario/SistemasBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brasil.devlops.sistemas_bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
